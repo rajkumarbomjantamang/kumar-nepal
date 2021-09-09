03bc68e1d13ba001e7a70477871fac88bb63aa67
rajkumarbomjan <rajvision100@gmail.com>
2021-09-08 20:07:54 -0700

    day 1 and day two codes : codes are Hello world and Greeting

---
+++ src/main/java/day2/Greeting.java
@@ -0,0 +1,9 @@
+package day2;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello everyone ");
+        System.out.println("good to see you");
+        System.out.println("\nhello how are you?\nhi where are you going\ni am java class student\nwould like to join us");
+    }
+}
